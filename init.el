c2ff1bb8ad38914cf59437aeac5267062534a912
Simon <contact@saimon.org>
2012-03-21 00:52:53 +0100

    add some packages: magit, zenburn, markdown, rainbow, less, lua

--- init.el
+++ init.el
@@ -8,9 +8,16 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-eshell starter-kit-js)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings
+                                  starter-kit-eshell starter-kit-js
+                                  zenburn-theme
+                                  magit magithub markdown-mode rainbow-mode
+                                  less-css-mode lua-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+(load-theme 'zenburn t)
+
