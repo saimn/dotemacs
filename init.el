516a397026ecc9decb69059635e068654369d997
Simon <contact@saimon.org>
2012-07-20 10:14:14 +0200

    fix ack

--- init.el
+++ init.el
@@ -240,7 +240,6 @@
 (setq auto-compression-mode t)
 
 ;; Ack
-(add-to-list 'load-path "/path/to/ack-and-a-half")
 (autoload 'ack-and-a-half-same "ack-and-a-half" nil t)
 (autoload 'ack-and-a-half "ack-and-a-half" nil t)
 (autoload 'ack-and-a-half-find-file-same "ack-and-a-half" nil t)
