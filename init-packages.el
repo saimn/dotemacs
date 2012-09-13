62c34f34aab902dc34d2bae225430d4b77c3e568
Simon <contact@saimon.org>
2012-09-13 14:08:22 +0200

    switch to melpa

--- init-packages.el
+++ init-packages.el
@@ -1,7 +1,8 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             ;; '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
