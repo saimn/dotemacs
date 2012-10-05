46498c18aa213cb848421e1504fd5100424df385
Simon <contact@saimon.org>
2012-10-05 23:39:29 +0200

    web - cleanup & reorganize

--- init-web.el
+++ init-web.el
@@ -1,6 +1,11 @@
 ;;------------------------------------------------------------
 ;; PHP
 ;;------------------------------------------------------------
+
+(autoload 'php-mode "php-mode" "PHP editing mode" t)
+;; (autoload 'php-html-helper-mode "html-helper-mode" "html-helper-mode" t)
+;; (add-auto-mode 'php-mode "\\.php[345]?\\'\\|\\.phtml\\." "\\.(inc|tpl)$" "\\.module$")
+
 ;; Manuel php en français
 (setq php-manual-url "http://fr.php.net/manual/fr/")
 
@@ -13,25 +18,29 @@
   '(progn
      (define-key php-mode-map (kbd "RET") 'reindent-then-newline-and-indent)))
 
-;;------------------------------------------------------------
-;; HTML
-;;------------------------------------------------------------
-;; Indenter automatiquement lorsque l'on appuie sur entrée
-(defun my-html-helper-load-hook ()
-  (define-key html-mode-map (kbd "RET") 'newline-and-indent))
-(add-hook 'html-helper-load-hook 'my-html-helper-load-hook)
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . html-mode))
 
 ;;------------------------------------------------------------
-;; Css
+;; CSS
 ;;------------------------------------------------------------
+
+(autoload 'rainbow-mode "rainbow-mode" "Highlight color names in buffer" t)
+(autoload 'css-mode "css-mode" "CSS editing mode" t)
+
 (defun my-css-mode-hook ()
   (setq css-indent-offset 2)
   (define-key css-mode-map "\C-c s" 'css-insert-section)
   (autopair-mode -1)
-  (rainbow-mode 1))
+  (rainbow-mode 1)
+  (setq scss-sass-command (expand-file-name "~/.gem/ruby/1.9.1/bin/sass"))
+  (setq scss-sass-options (quote ("--debug-info"))))
 
 (add-hook 'css-mode-hook 'my-css-mode-hook)
 (add-hook 'scss-mode-hook 'my-css-mode-hook)
+
+(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
 (defun css-insert-section (section)
   "Inserts a css section."
@@ -42,8 +51,11 @@
   (insert (concat "*/\n" "\n")))
 
 ;;------------------------------------------------------------
-;; Js
+;; JS
 ;;------------------------------------------------------------
+
+(autoload 'javascript-mode "javascript-mode.el" nil t)
+
 (defun my-js-mode-hook ()
   (setq js-indent-level 2)
   ;; fixes problem with pretty function font-lock
@@ -56,23 +68,38 @@
 
 (add-hook 'js-mode-hook 'my-js-mode-hook)
 
+(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . javascript-mode))
+
+;;------------------------------------------------------------
+;; HTML
 ;;------------------------------------------------------------
 
 (require 'mmm-auto)
-(setq mmm-global-mode 'maybe)
+;; (setq mmm-global-mode 'maybe)
 (mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
-(mmm-add-mode-ext-class 'css-mode "\\.html\\'" 'css-mode-html)
-(mmm-add-mode-ext-class 'js2-mode "\\.html\\'" 'js2-mode-html)
+(mmm-add-mode-ext-class 'html-mode nil 'html-js)
+(mmm-add-mode-ext-class 'html-mode nil 'embedded-css)
+;; (mmm-add-mode-ext-class 'css-mode "\\.html\\'" 'css-mode-html)
+;; (mmm-add-mode-ext-class 'js2-mode "\\.html\\'" 'js2-mode-html)
 
-;; Utiliser PSGML pour les fichiers SGML, HTML, XML
-;; (autoload 'sgml-mode "psgml" "Major mode to edit SGML files." t)
-;; (autoload 'xml-mode "psgml" "Major mode to edit XML files." t)
-(autoload 'rainbow-mode "rainbow-mode" "Highlight color names in buffer" t)
-(autoload 'css-mode "css-mode" "CSS editing mode" t)
-(autoload 'javascript-mode "javascript-mode.el" nil t)
-(autoload 'php-html-helper-mode "html-helper-mode" "html-helper-mode" t)
-(autoload 'php-mode "php-mode" "PHP editing mode" t)
-;; (add-auto-mode 'php-mode "\\.php[345]?\\'\\|\\.phtml\\." "\\.(inc|tpl)$" "\\.module$")
+;; Indenter automatiquement lorsque l'on appuie sur entrée
+;; (defun my-html-helper-load-hook ()
+;;   (define-key html-mode-map (kbd "RET") 'newline-and-indent))
+;; (add-hook 'html-helper-load-hook 'my-html-helper-load-hook)
+
+(setq auto-mode-alist
+      (append '(("\\.x[ms]l\\'" . nxml-mode)
+                ;; ("\\.[sx]?html?\\'" . html-mumamo)
+                ;; ("\\.tpl\\'" . smarty-html-mumamo)
+                ("\\.[sx]?html?\\'" . html-mode)
+                ("\\.tpl\\'" . html-mode)
+                ("\\.sql\\'" . sql-mode))
+              auto-mode-alist))
+
+;;------------------------------------------------------------
+;; NXHTML & MUMAMO
+;;------------------------------------------------------------
 
 ;; (load "~/.emacs.d/site-lisp/nxhtml/autostart.el")
 ;; (autoload 'html-mumamo "~/.emacs.d/site-lisp/nxhtml/autostart.el")
@@ -90,25 +117,5 @@
 ;;  indent-region-mode t
 ;;  rng-nxml-auto-validate-flag nil
 ;;  nxml-degraded t)
-
-(setq auto-mode-alist
-      (append '(("\\.x[ms]l\\'" . nxml-mode)
-                ;; ("\\.[sx]?html?\\'" . html-mumamo)
-                ;; ("\\.tpl\\'" . smarty-html-mumamo)
-                ;; ("\\.php\\'" . html-mumamo)
-                ;; ("\\.inc\\'" . html-mumamo)
-                ("\\.sql\\'" . sql-mode)
-                ("\\.css\\'" . css-mode)
-                ("\\.scss\\'" . scss-mode)
-                ("\\.js\\'" . javascript-mode)
-                ("\\.json\\'" . javascript-mode))
-              auto-mode-alist))
-
-
-;; (require 'zencoding-mode)
-;; (add-hook 'html-mumamo-mode-hook 'zencoding-mode)
-;; (define-key html-mumamo-mode-map (kbd "C-j") 'zencoding-expand-line)
-;; (define-key smarty-html-mumamo-mode-map (kbd "C-j") 'zencoding-expand-line)
-
 
 (provide 'init-web)
